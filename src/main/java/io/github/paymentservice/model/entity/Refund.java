8b4dfb2f659202cb9f1533dc1febf03c7f0da71d
jesusdangerous <aleksandrzonov641@gmail.com>
2025-06-27 17:23:58 +0500

    feat: add refund entity

---
+++ src/main/java/io/github/paymentservice/model/entity/Refund.java
@@ -0,0 +1,31 @@
+package io.github.paymentservice.model.entity;
+
+import io.github.paymentservice.model.enums.RefundStatus;
+import io.github.paymentservice.model.enums.converter.RefundStatusConverter;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Refund extends BaseEntity {
+    private BigDecimal refundedAmount;
+    private String reason;
+
+    @Convert(converter = RefundStatusConverter.class)
+    private RefundStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "paymentTransactionId", referencedColumnName = "id")
+    private PaymentTransaction paymentTransaction;
+}
