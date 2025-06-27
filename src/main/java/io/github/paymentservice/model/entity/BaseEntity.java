bf37adc6c90bfbb93d8253795211b056c9c38249
jesusdangerous <aleksandrzonov641@gmail.com>
2025-06-27 14:57:23 +0500

    feat: add bank account entity

--- src/main/java/io/github/paymentservice/model/entity/BaseEntity.java
+++ src/main/java/io/github/paymentservice/model/entity/BaseEntity.java
@@ -1,9 +1,6 @@
 package io.github.paymentservice.model.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.Version;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -15,6 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class BaseEntity {
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
