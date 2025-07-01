41ce142a184c38ffb2cc3ad8342d5b5b6bc60f2e
jesusdangerous <aleksandrzonov641@gmail.com>
2025-07-01 19:13:50 +0500

    feat: add json converter

---
+++ src/main/java/io/github/paymentservice/util/JsonConverter.java
@@ -0,0 +1,22 @@
+package io.github.paymentservice.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.paymentservice.model.dto.CreatePaymentTransactionRequest;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@UtilityClass
+public class JsonConverter {
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public <T> T toObject(String json, Class<T> clazz) {
+        try {
+            return mapper.readValue(json, clazz);
+        } catch (JsonProcessingException e) {
+            log.error("Json deserializing exception: {}" ,e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
