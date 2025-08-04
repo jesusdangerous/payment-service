49550ba1c71eb9c37e88586279d11bd43911c762
jesusdangerous <aleksandrzonov641@gmail.com>
2025-08-04 19:53:14 +0500

    feat: add bank account repository

---
+++ src/main/java/io/github/paymentservice/repository/BankAccountRepository.java
@@ -0,0 +1,9 @@
+package io.github.paymentservice.repository;
+
+import io.github.paymentservice.model.entity.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
+}
